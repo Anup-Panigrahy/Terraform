a0a3857cdda4d6e55e782d0c0d44956bb434ba24
AnupIT <100036860+Anup-Panigrahy@users.noreply.github.com>
2026-04-26 09:54:26 +0530

    Update RDS_mysql_database_creation_By_Terraform_Script.tf

--- RDS_mysql_database_creation_By_Terraform_Script.tf
+++ RDS_mysql_database_creation_By_Terraform_Script.tf
@@ -13,3 +13,215 @@ resource "aws_db_instance" "default" {
        skip_final_snapshot = true
 
 }
+----------
+[ec2-user@ip-172-31-38-29 ex-6]$ terraform init
+Initializing the backend...
+Initializing provider plugins...
+- Reusing previous version of hashicorp/aws from the dependency lock file
+- Using previously-installed hashicorp/aws v6.42.0
+
+Terraform has been successfully initialized!
+
+You may now begin working with Terraform. Try running "terraform plan" to see
+any changes that are required for your infrastructure. All Terraform commands
+should now work.
+
+If you ever set or change modules or backend configuration for Terraform,
+rerun this command to reinitialize your working directory. If you forget, other
+commands will detect it and remind you to do so if necessary.
+[ec2-user@ip-172-31-38-29 ex-6]$ terraform validate
+Success! The configuration is valid.
+
+[ec2-user@ip-172-31-38-29 ex-6]$ terraform fmt
+RDS_mysql_database_creation_By_Terraform_Script.tf
+[ec2-user@ip-172-31-38-29 ex-6]$ terraform plan
+
+Terraform used the selected providers to generate the following execution plan. Resource actions are indicated with the following symbols:
+  + create
+
+Terraform will perform the following actions:
+
+  # aws_db_instance.default will be created
+  + resource "aws_db_instance" "default" {
+      + address                               = (known after apply)
+      + allocated_storage                     = 100
+      + apply_immediately                     = false
+      + arn                                   = (known after apply)
+      + auto_minor_version_upgrade            = true
+      + availability_zone                     = (known after apply)
+      + backup_retention_period               = (known after apply)
+      + backup_target                         = (known after apply)
+      + backup_window                         = (known after apply)
+      + ca_cert_identifier                    = (known after apply)
+      + character_set_name                    = (known after apply)
+      + copy_tags_to_snapshot                 = false
+      + database_insights_mode                = (known after apply)
+      + db_name                               = "mysqldb"
+      + db_subnet_group_name                  = (known after apply)
+      + dedicated_log_volume                  = false
+      + delete_automated_backups              = true
+      + domain_fqdn                           = (known after apply)
+      + endpoint                              = (known after apply)
+      + engine                                = "mysql"
+      + engine_lifecycle_support              = (known after apply)
+      + engine_version                        = "8.4.8"
+      + engine_version_actual                 = (known after apply)
+      + hosted_zone_id                        = (known after apply)
+      + id                                    = (known after apply)
+      + identifier                            = (known after apply)
+      + identifier_prefix                     = (known after apply)
+      + instance_class                        = "db.t4g.micro"
+      + iops                                  = (known after apply)
+      + kms_key_id                            = (known after apply)
+      + latest_restorable_time                = (known after apply)
+      + license_model                         = (known after apply)
+      + listener_endpoint                     = (known after apply)
+      + maintenance_window                    = (known after apply)
+      + master_user_secret                    = (known after apply)
+      + master_user_secret_kms_key_id         = (known after apply)
+      + monitoring_interval                   = 0
+      + monitoring_role_arn                   = (known after apply)
+      + multi_az                              = (known after apply)
+      + nchar_character_set_name              = (known after apply)
+      + network_type                          = (known after apply)
+      + option_group_name                     = (known after apply)
+      + parameter_group_name                  = (known after apply)
+      + password                              = (sensitive value)
+      + password_wo                           = (write-only attribute)
+      + performance_insights_enabled          = false
+      + performance_insights_kms_key_id       = (known after apply)
+      + performance_insights_retention_period = (known after apply)
+      + port                                  = (known after apply)
+      + publicly_accessible                   = false
+      + region                                = "ap-south-1"
+      + replica_mode                          = (known after apply)
+      + replicas                              = (known after apply)
+      + resource_id                           = (known after apply)
+      + skip_final_snapshot                   = true
+      + snapshot_identifier                   = (known after apply)
+      + status                                = (known after apply)
+      + storage_throughput                    = (known after apply)
+      + storage_type                          = (known after apply)
+      + tags_all                              = (known after apply)
+      + timezone                              = (known after apply)
+      + upgrade_rollout_order                 = (known after apply)
+      + username                              = "admin"
+      + vpc_security_group_ids                = (known after apply)
+    }
+
+Plan: 1 to add, 0 to change, 0 to destroy.
+
+──────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
+
+Note: You didn't use the -out option to save this plan, so Terraform can't guarantee to take exactly these actions if you run "terraform apply" now.
+[ec2-user@ip-172-31-38-29 ex-6]$ terraform apply -auto-approve
+
+Terraform used the selected providers to generate the following execution plan. Resource actions are indicated with the following symbols:
+  + create
+
+Terraform will perform the following actions:
+
+  # aws_db_instance.default will be created
+  + resource "aws_db_instance" "default" {
+      + address                               = (known after apply)
+      + allocated_storage                     = 100
+      + apply_immediately                     = false
+      + arn                                   = (known after apply)
+      + auto_minor_version_upgrade            = true
+      + availability_zone                     = (known after apply)
+      + backup_retention_period               = (known after apply)
+      + backup_target                         = (known after apply)
+      + backup_window                         = (known after apply)
+      + ca_cert_identifier                    = (known after apply)
+      + character_set_name                    = (known after apply)
+      + copy_tags_to_snapshot                 = false
+      + database_insights_mode                = (known after apply)
+      + db_name                               = "mysqldb"
+      + db_subnet_group_name                  = (known after apply)
+      + dedicated_log_volume                  = false
+      + delete_automated_backups              = true
+      + domain_fqdn                           = (known after apply)
+      + endpoint                              = (known after apply)
+      + engine                                = "mysql"
+      + engine_lifecycle_support              = (known after apply)
+      + engine_version                        = "8.4.8"
+      + engine_version_actual                 = (known after apply)
+      + hosted_zone_id                        = (known after apply)
+      + id                                    = (known after apply)
+      + identifier                            = (known after apply)
+      + identifier_prefix                     = (known after apply)
+      + instance_class                        = "db.t4g.micro"
+      + iops                                  = (known after apply)
+      + kms_key_id                            = (known after apply)
+      + latest_restorable_time                = (known after apply)
+      + license_model                         = (known after apply)
+      + listener_endpoint                     = (known after apply)
+      + maintenance_window                    = (known after apply)
+      + master_user_secret                    = (known after apply)
+      + master_user_secret_kms_key_id         = (known after apply)
+      + monitoring_interval                   = 0
+      + monitoring_role_arn                   = (known after apply)
+      + multi_az                              = (known after apply)
+      + nchar_character_set_name              = (known after apply)
+      + network_type                          = (known after apply)
+      + option_group_name                     = (known after apply)
+      + parameter_group_name                  = (known after apply)
+      + password                              = (sensitive value)
+      + password_wo                           = (write-only attribute)
+      + performance_insights_enabled          = false
+      + performance_insights_kms_key_id       = (known after apply)
+      + performance_insights_retention_period = (known after apply)
+      + port                                  = (known after apply)
+      + publicly_accessible                   = false
+      + region                                = "ap-south-1"
+      + replica_mode                          = (known after apply)
+      + replicas                              = (known after apply)
+      + resource_id                           = (known after apply)
+      + skip_final_snapshot                   = true
+      + snapshot_identifier                   = (known after apply)
+      + status                                = (known after apply)
+      + storage_throughput                    = (known after apply)
+      + storage_type                          = (known after apply)
+      + tags_all                              = (known after apply)
+      + timezone                              = (known after apply)
+      + upgrade_rollout_order                 = (known after apply)
+      + username                              = "admin"
+      + vpc_security_group_ids                = (known after apply)
+    }
+
+Plan: 1 to add, 0 to change, 0 to destroy.
+aws_db_instance.default: Creating...
+aws_db_instance.default: Still creating... [00m10s elapsed]
+aws_db_instance.default: Still creating... [00m20s elapsed]
+aws_db_instance.default: Still creating... [00m30s elapsed]
+aws_db_instance.default: Still creating... [00m40s elapsed]
+aws_db_instance.default: Still creating... [00m50s elapsed]
+aws_db_instance.default: Still creating... [01m00s elapsed]
+aws_db_instance.default: Still creating... [01m10s elapsed]
+aws_db_instance.default: Still creating... [01m20s elapsed]
+aws_db_instance.default: Still creating... [01m30s elapsed]
+aws_db_instance.default: Still creating... [01m40s elapsed]
+aws_db_instance.default: Still creating... [01m50s elapsed]
+aws_db_instance.default: Still creating... [02m00s elapsed]
+aws_db_instance.default: Still creating... [02m10s elapsed]
+aws_db_instance.default: Still creating... [02m20s elapsed]
+aws_db_instance.default: Still creating... [02m30s elapsed]
+aws_db_instance.default: Still creating... [02m40s elapsed]
+aws_db_instance.default: Still creating... [02m50s elapsed]
+aws_db_instance.default: Still creating... [03m00s elapsed]
+aws_db_instance.default: Still creating... [03m10s elapsed]
+aws_db_instance.default: Still creating... [03m20s elapsed]
+aws_db_instance.default: Still creating... [03m30s elapsed]
+aws_db_instance.default: Still creating... [03m40s elapsed]
+aws_db_instance.default: Still creating... [03m50s elapsed]
+aws_db_instance.default: Still creating... [04m00s elapsed]
+aws_db_instance.default: Still creating... [04m10s elapsed]
+aws_db_instance.default: Still creating... [04m20s elapsed]
+aws_db_instance.default: Still creating... [04m30s elapsed]
+aws_db_instance.default: Still creating... [04m40s elapsed]
+aws_db_instance.default: Still creating... [04m50s elapsed]
+aws_db_instance.default: Creation complete after 4m56s [id=db-R42AFOHY2MAVVH2FFPGU2N3OFU]
+
+Apply complete! Resources: 1 added, 0 changed, 0 destroyed.
+[ec2-user@ip-172-31-38-29 ex-6]$
+
