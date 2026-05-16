4a09daeee4576630d3e74d350813cb716e0886f8
Anup-Panigrahy <anup.kumarpanigrahy12@gmail.com>
2026-05-16 23:03:11 +0530

    16-05-2016

---
+++ Terraform_behaviour.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "my_ec2_instance" {
+  ami           = "ami-07a00cf47dbbc844c"
+  instance_type = "t3.small"
+  tags = {
+    Name = "MyEC2Instance for checking terraform behaviour"
+  }
+}
