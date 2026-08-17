95fac8388447a6b762f9fd39c7d50eb82839fab8
AnupIT <100036860+Anup-Panigrahy@users.noreply.github.com>
2026-08-17 08:13:35 +0530

    Creation of EC2 instance along with Elastic IP and association of EC2 instance with Elastic IP

--- creation_elastic_ip.tf
+++ creation_elastic_ip.tf
@@ -15,6 +15,6 @@ resource "aws_eip" "my_eip" {
   domain   = "vpc"
 }
 
-output "instance_public_ip" {
+output "instance_public_ip" {                                        // This block is required for getting output named as "instance_public_ip"
   value = aws_eip.my_eip.public_ip
 }
