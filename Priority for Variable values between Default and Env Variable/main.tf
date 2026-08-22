99cdce08462bc55f806780cfaefb49ea81791e1e
AnupIT <100036860+Anup-Panigrahy@users.noreply.github.com>
2026-08-22 08:39:32 +0530

    Clarify variable precedence in Terraform configuration
    Added comments explaining variable precedence in Terraform.

--- Priority for Variable values between Default and Env Variable/main.tf
+++ Priority for Variable values between Default and Env Variable/main.tf
@@ -2,3 +2,8 @@ resource "aws_instance" "variable_precedence" {
   ami           = "ami-0011550b539717e2a"
   instance_type = var.instance_type
 }
+
+// I've set up my Environment variable(by Edit the system Environment Variables in my system) as TF_VAR_instance_type=t3.large, that's why when I am running terraform plan and terraform apply -> 
+   I am getting as t3.large in insatnce_type
+   Because
+   Environment Variable value gets highest precedence/priority over Default value assigned to variable in variables.tf file.
