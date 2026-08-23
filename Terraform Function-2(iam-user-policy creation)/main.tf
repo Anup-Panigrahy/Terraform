77a8c4df4c5a581b59d2b3a5c66de725752b57e9
AnupIT <100036860+Anup-Panigrahy@users.noreply.github.com>
2026-08-23 05:57:05 +0530

    Attach IAM user policy to Zeal-Vora
    Added IAM user policy attachment for user Zeal-Vora using a JSON file.

--- Terraform Function-2(iam-user-policy creation)/main.tf
+++ Terraform Function-2(iam-user-policy creation)/main.tf
@@ -12,3 +12,7 @@ resource "aws_iam_user_policy" "Zeal_Vora_Policy" {
 
   policy = file("./iam-user-policy.json")  // Here file function has been used.
 }
+
+
+// I can see this user and see that this policy(demo-user-policy) also been attched with this user -> Zeal-Vora.
+// If I will open that policy , I can see that JSON what I have written in iam-user-policy.json
