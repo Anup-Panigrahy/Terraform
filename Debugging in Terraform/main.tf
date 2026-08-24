033700ba254bdadf17dcd19339c32fa6f8f811e4
AnupIT <100036860+Anup-Panigrahy@users.noreply.github.com>
2026-08-24 08:44:34 +0530

    Create main.tf

---
+++ Debugging in Terraform/main.tf
@@ -0,0 +1,15 @@
+locals {
+  common_tags = {
+    Team = "DevOps"
+  }
+}
+
+resource "aws_security_group" "SecurityGroup01" {
+  name = "SecurityGroup01"
+  tags = local.common_tags
+}
+
+resource "aws_security_group" "SecurityGroup02" {
+  name = "SecurityGroup02"
+  tags = local.common_tags
+}
